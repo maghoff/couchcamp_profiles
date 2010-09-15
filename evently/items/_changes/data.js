39f15589ebf99b0f9bcb51ea78c675a27841c2da
Chris Anderson <jchris@apache.org>
2010-09-15 12:25:54 -0700

    selenas changes

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -1,21 +1,39 @@
 function(data) {
-  var db = $$(this).app.db;
-  function attachments(doc) {
-    var as = [], a = doc._attachments;
-    if (a) {
-      for (var n in a) {
-        as.push({name: n, path: ["",db.name,doc._id,n].join('/')});
-      }
-      return as;
-    }
-  };
-  var p;
+
+// 	$.log(data);
+	var items = [];
+  for (var i=0; i < data.rows.length; i++) {
+	  var r = data.rows[i].value, item = {};
+		item.bio = r.bio || "";
+		item.urls = [];
+		item.name = data.rows[i].id;
+	
+		if (r.url) {
+				item.urls.push({url:r.url, label:"www"});
+		}
+
+// TODO: handle those people who handed us STRINGS or ARRAYS
+		if (r.urls) {
+			  if (typeof(r.urls) == 'string') {
+					item.urls.push({url:r.urls, label:"www"});
+				} else {
+					var u = r.urls;
+					for (var k in u) {
+						item.urls.push({url:u[k], label:k});
+					}
+				}
+		}
+
+		if (r._attachments) {
+			item.attachments = [];
+			var v = r._attachments;
+			for (var w in v) {
+				item.attachments.push({src:[encodeURIComponent(data.rows[i].id), encodeURIComponent(w)].join('/')});
+			}
+		}
+		items.push(item);
+	};
   return {
-    items : data.rows.map(function(r) {
-      p = r.value.profile;
-      p.message = r.value.message;
-      p.atts = attachments(r.value);
-      return p;
-    })
-  }
+    items: items
+	};
 };
